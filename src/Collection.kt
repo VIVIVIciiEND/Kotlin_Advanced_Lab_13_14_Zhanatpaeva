5a15de5362d70292bab2e7ef9edd4b78728fab8c
VIVIVIciiEND <akirmandinara@gmail.com>
2026-02-09 14:41:57 +0300

    Map collection in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -3,23 +3,38 @@ val gasPlanets = arrayOf("Jupiter" , "Saturn" , " Uranus" , " Neptune")
 val solarSystem = rockPlanets+ gasPlanets
 fun main()
 {
-    val solarSystem = mutableListOf("Mercury" , "Venus" , "Earth" , " Mars","Jupiter" , "Saturn" , " Uranus" , " Neptune" )
-    for ( planet in solarSystem){
-        println(planet)
-    }
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0 ,
+        "Venus" to 0,
+        "Earth" to 1,
+        " Mars" to 2,
+        "Jupiter" to 79
+        , "Saturn" to 82
+        , " Uranus" to 27
+        , " Neptune" to 14)
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    solarSystem["Pluto"]=5
+//    println(solarSystem.size)
+//    println(solarSystem.get("Theia"))
+//    for ( planet in solarSystem){
+//        println(planet)
+//    }
 //    solarSystem.add("Pluto")
 //    solarSystem.add(4,"Theia")
 //    solarSystem[3] = "Future Moon"
 //    println(solarSystem.contains("Pluto"))
 //    println("Future Moon" in solarSystem)
-    println(solarSystem.size)
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
-    println("Pluto" in solarSystem)
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
-    solarSystem.remove("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+//    println(solarSystem.size)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+//    println("Pluto" in solarSystem)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
 }
