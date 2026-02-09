90a6f695d008c5890bd8641519a89bbeb3cf70a7
VIVIVIciiEND <akirmandinara@gmail.com>
2026-02-09 14:37:05 +0300

    Sets in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -7,9 +7,19 @@ fun main()
     for ( planet in solarSystem){
         println(planet)
     }
+//    solarSystem.add("Pluto")
+//    solarSystem.add(4,"Theia")
+//    solarSystem[3] = "Future Moon"
+//    println(solarSystem.contains("Pluto"))
+//    println("Future Moon" in solarSystem)
+    println(solarSystem.size)
     solarSystem.add("Pluto")
-    solarSystem.add(4,"Theia")
-    solarSystem[3] = "Future Moon"
+    println(solarSystem.size)
     println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
+    println("Pluto" in solarSystem)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
 }
